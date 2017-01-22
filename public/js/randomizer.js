398ab8ae4134478dc9a53649d9e7761dd9401abe
jhoun <jayhoun@gmail.com>
2017-01-21 20:40:22 -1000

    randomizer

--- public/js/randomizer.js
+++ public/js/randomizer.js
@@ -1,8 +1,10 @@
-var adList = [
-  {path: "geico"},
-  {path: "viagra"},
-  {path: "shamwow"},
-]
+// var adList = [
+//   {path: "geico"},
+//   {path: "viagra"},
+//   {path: "shamwow"},
+// ]
+
+var pending = [];
 
 var workstation = {
   ws1Ad: null,
@@ -11,21 +13,23 @@ var workstation = {
   ws4Ad: null
 }
 
-
 function randomAd() {
  let ad = adList[Math.floor(Math.random()*adList.length)];
- return ad;
+ pending.push(ad);
 }
 
+for(var i = 0; i < 20; i++){
+  randomAd();
+}
 
+console.log('pending: ', pending);
 var sendAdToPhaser = function (obj) {
     var keys = Object.keys(obj)
     var prop = keys[keys.length * Math.random() << 0]
     if(obj[prop] === null){
-      prop = randomAd();
-      console.log('prop: ', prop);
+      obj[prop] = randomAd();
     }
 };
 
 
-setInterval(function(){sendAdToPhaser(workstation)}, 500);
+sendAdToPhaser(workstation);
