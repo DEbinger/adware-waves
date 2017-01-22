5e2232ba7f2019fbef3f570d8553e32aeb27e6c5
Steven Cable <steven.cable12@gmail.com>
2017-01-21 15:03:52 -1000

    created adList array of objects, and addAd function

---
+++ public/js/adList.js
@@ -0,0 +1,21 @@
+var adList = [
+
+];
+
+function addAd (path, name, numerator, denominator, kill, crash, loaded){
+  var newAd = {
+
+    path : path,
+    name : name,
+    n: numerator,
+    d: denominator,
+    kill : kill,
+    crash : crash,
+    loaded : loaded};
+
+  adList.push(newAd);
+
+}
+
+
+
