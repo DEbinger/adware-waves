40c18f2ee1d5b9598624b622bd79f00acaab0b73
David <davidebinger888@gmail.com>
2017-01-21 15:47:56 -1000

    resized images

--- public/js/adList.js
+++ public/js/adList.js
@@ -1,21 +1,44 @@
 var adList = [
 
+
 ];
 
-function addAd (path, name, numerator, denominator, kill, crash, loaded){
+function addAd (path, name, loaded){
   var newAd = {
 
     path : path,
     name : name,
-    n: numerator,
-    d: denominator,
-    kill : kill,
-    crash : crash,
+    n: null,
+    d: null,
+    kill : false,
+    crash : false,
     loaded : loaded};
 
   adList.push(newAd);
 
 }
+
+addAd('../assets/538.png','538',['']);
+addAd('../assets/HS.png','WOW',['']);
+addAd('../assets/Overwatch-Origins.png','Overwatch',['']);
+addAd('../assets/amazon.png','Amazon',['']);
+addAd('../assets/apple.png','Apple',['']);
+addAd('../assets/arrow.png','CW/Arrow',['']);
+addAd('../assets/bleh.png','Drumpf',['']);
+addAd('../assets/chrome.png','Chrome',['']);
+addAd('../assets/cialis.png','Cialis',['']);
+addAd('../assets/crazy.png','CW/CrazyExGirlfriend',['']);
+addAd('../assets/crown.png','CrownRoyal',['']);
+addAd('../assets/devleague.png','Devleague',['']);
+addAd('../assets/dorito.png','Doritos',['']);
+addAd('../assets/geico.png','Geico',['']);
+addAd('../assets/halo.png','Halo',['']);
+addAd('../assets/mario.png','MarioKart',['']);
+addAd('../assets/mcd.png','McDonalds',['']);
+addAd('../assets/netflix.png','NetflixAndChill',['']);
+addAd('../assets/ps.png','Playstation',['']);
+addAd('../assets/toyota.png','Toyota',['']);
+
 
 
 
