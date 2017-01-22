4c22d96599b91898948993826cd30ef5a7465796
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-01-21 21:59:25 -1000

    created fields to display pending array in browser

---
+++ public/js/adPending.js
@@ -0,0 +1,29 @@
+// const nextAd = pending[0];
+// const adPostion1 = pending[1];
+// const adPostion2 = pending[2];
+// const adPostion3 = pending[3];
+
+let nextContainer = document.getElementById('next');
+
+nextContainer.innerHTML = `<img src="public/${pending[0].path}"><img>`;
+const adPostion1 = document.createElement('div');
+
+let backlogContainer= document.getElementById('backlog');
+
+let adPostion1Container = document.createElement('div');
+adPostion1Container.id = "adPostion1";
+backlogContainer.appendChild(adPostion1Container);
+adPostion1Container.innerHTML = `<img src="public/${pending[1].path}"><img>`;
+
+
+
+let adPostion2Container = document.createElement('div');
+adPostion2Container.id = "adPostion2";
+backlogContainer.appendChild(adPostion2Container);
+adPostion2Container.innerHTML = `<img src="public/${pending[2].path}"><img>`;
+
+
+let adPostion3Container = document.createElement('div');
+adPostion3Container.id = "adPostion3";
+backlogContainer.appendChild(adPostion3Container);
+adPostion3Container.innerHTML = `<img src="public/${pending[3].path}"><img>`;
