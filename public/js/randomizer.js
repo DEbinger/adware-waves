dda57812385b8a675e5c6091b23e406bd9d07e48
David <davidebinger888@gmail.com>
2017-01-22 14:01:50 -1000

    fade in with phaser and randomizer time change

--- public/js/randomizer.js
+++ public/js/randomizer.js
@@ -1,6 +1,6 @@
 var level = 50; // leave it at 50 for now to make things go faster
 var pending = [];
-var numOfAd = 5 + 5 * level;
+var numOfAd;
 var timePerAd;
 var workstation = {
   ws1Ad: null,
@@ -9,13 +9,14 @@ var workstation = {
   ws4Ad: null
 };
 
+calcTimePerAd();
+
 for(let i = 0; i < numOfAd; i++) {
   pending.push(adList[Math.floor(Math.random()*adList.length)]);
 }
 
 function calcTimePerAd() {
+  numOfAd = 5 + 5 * level;
   timePerAd = 300 / numOfAd;
   timePerAd = Math.floor(timePerAd);
 }
-
-calcTimePerAd();
